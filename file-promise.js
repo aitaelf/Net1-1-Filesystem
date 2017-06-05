585c29e9d50c1e63ae8dd16e3475f6c69eda6e6e
aitaelf <aitaelf@mail.ru>
2017-06-05 23:55:54 +0300

    1

---
+++ file-promise.js
@@ -0,0 +1,33 @@
+﻿const fs = require('fs');
+const conf = { enconing: 'utf8' };
+
+let read = (file) => {
+	return new Promise((done, fail) => {
+		fs.readFile(file, conf, (err, content) => {
+			if(err) {
+				fail(err);
+			} else {
+				done(content.toString());
+			}
+		})	
+	})
+	
+}
+
+let write = (file, data) => {
+	return new Promise((done, fail) => {
+		fs.writeFile(file, data, err => {
+			if (err) {
+				fail(err);
+			} else {
+				done(file);
+			}
+		})	
+	})
+	
+}
+
+module.exports = {
+	read,
+	write
+}
