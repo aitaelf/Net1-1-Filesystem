1152422b3239bd40304ff8769fc9a610ff908f3b
aitaelf <aitaelf@mail.ru>
2017-06-06 01:53:00 +0300

    2

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-﻿const file = require('./file-promise');
+﻿// 1
+
+const file = require('./file-promise');
 
 file
   .read('./data.txt')
   .then(data => data.toUpperCase())
   .then(data => file.write('./upper-data.txt', data))
   .then(filename => console.log(`Создан файл ${filename}`))
+  .catch(err => console.error(err));
+
+// 2
+
+const readAll = require('./read-all');
+
+function show(file) {
+  console.log('-'.repeat(10));
+  console.log(`Содержимое файлы ${file.name}:`);
+  console.log(file.content);
+  console.log('-'.repeat(10));
+}
+
+readAll('./logs/')
+  .then(files => files.forEach(show))
   .catch(err => console.error(err));
---
+++ read-all.js
@@ -0,0 +1,30 @@
+﻿const fs = require('fs');
+const conf = { enconing: 'utf8' };
+
+module.exports = (path) => {
+	return new Promise((done, fail) => {
+		fs.readdir(path, (err, files) => {
+			if(err) {
+				fail(err);
+			} else {
+				let result = [];
+				files.forEach(file => {
+					result.push(readFile(path + file));
+				});
+				done(Promise.all(result));
+			}
+		});
+	});
+}
+
+function readFile(file) {
+	return new Promise((done, fail) => {
+		fs.readFile(file, conf, (err, content) => {
+			if (err) {
+				fail(err);
+			} else {
+				done( { name: file, content: content.toString()} );
+			}
+		});
+	});
+}
